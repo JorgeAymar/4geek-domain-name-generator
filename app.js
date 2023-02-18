c19438ec8ba1c0acabff82ca2ccca749224467da
Jorge Sánchez Aymar <90512447+JorgeAymar@users.noreply.github.com>
2023-02-18 14:08:24 +0000

    last execution

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let ext = ["com", "net", "us", "io"];
 
-function getWord(arr) {
-  let i = arr.length;
-  x = Math.floor(Math.random() * i);
-  return arr[x];
+console.log("**********");
+
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let e = 0; e < ext.length; e++) {
+        console.log(pronoun[p] + adj[a] + noun[n] + "." + ext[e]);
+      }
+    }
+  }
 }
 
-for (let i = 0; i < 8; i++) {
-  console.log(
-    getWord(pronoun) + getWord(adj) + getWord(noun) + "." + getWord(ext)
-  );
-}
+console.log("**********");
